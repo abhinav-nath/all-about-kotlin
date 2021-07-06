b9e512216e0afe4704671bbff19fc87fecccb6d5
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-07-07 04:23:21 +0530

    added VariableDeclarations.kt

---
+++ src/com/codecafe/basics/VariableDeclarations.kt
@@ -0,0 +1,35 @@
+package com.codecafe.basics
+
+/*
+    Two types of variable declarations:
+
+    1. Using val - like final in Java
+    2. Using var - mutable variables
+
+    Kotlin recommends to use val variables to promote immutability.
+ */
+
+fun main() {
+
+    // type will be inferred as Int
+    // mandatory to provide value in this case
+    // then only type inference will work
+    // if value is not provided then compilation error
+    val number1 = 25
+
+    // or we can specify the type
+    val number2: Int = 25
+
+    // initialization is not required in this case
+    // as we are mentioning the type here
+    val number3: Int
+
+    val shortNumber: Short = 30
+    // shortNumber = 50 -> compilation error as it is a final variable
+
+    // var variables are mutable
+    // compiler underlines the var variables
+    var number = 10
+    number = 20
+
+}
